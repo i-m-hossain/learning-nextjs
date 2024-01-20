453cc6cf60a7bc8bf0fdb8382b434a66bd0d2c91
mdimran1409036 <mdimran1409036@gmail.com>
2024-01-20 13:19:22 +0600

    dynamic meta data generation added

--- app/posts/[post]/page.tsx
+++ app/posts/[post]/page.tsx
@@ -1,13 +1,23 @@
 import getSinglePost from '@/lib/getSinglePost';
+import { Metadata, ResolvingMetadata } from 'next';
 import { notFound } from 'next/navigation';
 import React from 'react';
 
 type Props = {
   params: { post: string };
 };
+//dynamic metadata
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const id = params.post;
+  const post = await getSinglePost(parseInt(id));
+  return {
+    title: post.title,
+    description: post.body,
+  };
+}
 
 export default async function SinglePost({ params }: Props) {
-  const { post: id } = params;
+  const id = params.post;
   // if (id == '3') {
   //   notFound();
   // }
