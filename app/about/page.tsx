85697d357c2e326ca07c5ef61ade5536cebdb5e1
mdimran1409036 <mdimran1409036@gmail.com>
2024-01-20 11:39:17 +0600

    image added with placeholder

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import image from '@/public/images/pro-pic.png';
+import Image from 'next/image';
 type Props = {};
 
 export default function About({}: Props) {
-  throw new Error('an error happened');
-  return <div>About</div>;
+  // throw new Error('an error happened');
+  return (
+    <div>
+      <Image src={image} alt='pro-pic' placeholder='blur' />
+    </div>
+  );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,15 +4,15 @@ import React from 'react';
 type Props = {};
 const routeList = [
   { name: 'Home', path: '/' },
-  { name: 'About', path: '/about' },
   { name: 'Posts', path: '/posts' },
+  { name: 'About', path: '/about' },
 ];
 const isLastMenuChild = (index: number, totalRoutes: number): boolean =>
   index !== totalRoutes - 1;
 
 export default function Sidebar({}: Props) {
   return (
-    <aside className='w-1/6 md:w-1/12  bg-gray-900 pt-6 pl-4 text-white'>
+    <aside className='w-1/6 md:w-1/12  bg-gray-900 pt-6 text-center text-white'>
       <ul className='flex flex-col gap-4'>
         {routeList.map((route, i) => (
           <Link href={route.path} key={route.name}>
